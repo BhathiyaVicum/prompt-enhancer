24bb4925247a2774f54d7c0e1f283feb842c4084
Bhathiya Vicum <bathiyavicum0@gmail.com>
2026-05-05 12:29:14 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <div className="header flex items-center justify-between p-4 bg-gray-800">
+      <header>
+        <h1 class="text-xl font-medium text-white">Prompt Perfector</h1>
+      </header>
+    </div>
+  )
+}
